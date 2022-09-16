c15405f3c3f92a917d44aa10f776879d3f073add
gilmar <gipajfm@gmail.com>
2022-09-16 18:40:09 -0500

    agregando estilos al container de los card

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -6,7 +6,7 @@ const LocationInfo = ({location}) => {
       <article className='card-info '>
           <h2 className='card-info--title'><span> </span>{location?.name}</h2>
           <div className='card-info__body '>
-            <h3 className='card-info__body--title  '><span>Type:</span>{location?.type}</h3>
+            <h3 className='card-info__body--title  '><span>Type: </span>{location?.type}</h3>
             <h3 className='card-info__body--dimension  '><span>Dimension: </span>{location?.dimension}</h3>
             <h3 className='card-info__body--population  '><span>Population: </span>{location?.residents.length}</h3>
           </div>      
--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -5,36 +5,42 @@ const ResidentInfo = ({url}) => {
     // console.log(resident);
 
     const assignStatus = () => {
-      let backgroundState= {};
-      (residents?.status.toLowerCase() === 'dead') ? backgroundState.backgroundColor = 'gray'
+      // let backgroundState= {};
+      // (residents?.status.toLowerCase() === 'dead') ? backgroundState.backgroundColor = 'gray'
+      //                                  : (residents?.status.toLowerCase() === 'unknown') 
+      //                                  ? backgroundState.backgroundColor = 'red'
+      //                                  : backgroundState.backgroundColor = 'green' 
+      let color= '';
+      (residents?.status.toLowerCase() === 'dead') ? color = 'gray'
                                        : (residents?.status.toLowerCase() === 'unknown') 
-                                       ? backgroundState.backgroundColor = 'red'
-                                       : backgroundState.backgroundColor = 'green' 
+                                       ? color = 'red'
+                                       : color = 'green' 
                                       
-       return backgroundState                             
+       return color                             
     }
 
     const residents = useFetch(url)
-    console.log(assignStatus())
-    console.log(residents?.status.toLowerCase())
-    console.log(typeof(residents?.status))
-    console.log(residents)
+    // console.log(assignStatus())
+    // console.log(residents?.status.toLowerCase())
+    // console.log(typeof(residents?.status))
+    // console.log(residents)
   return (
-      <article className='card-resident'>
-        <h5 className='card-resident--title'>{residents?.name}</h5>
+      <article style={{boxShadow : `0px 5px 15px ${assignStatus(residents?.status)}`}} className='card-resident'>
+        <h5 style={{color : assignStatus(residents?.status)}} className='card-resident--title'>{residents?.name}</h5>
         <header className='card-resident__header'>
             <img className='card-resident__header--img' src={residents?.image} alt="" />
           <div className='card-resident__header--state'>
-              <div className='card-resident__header--circle' style={assignStatus(residents?.status)}> </div>
+              {/* <div className='card-resident__header--circle' style={assignStatus(residents?.status)}> </div> */}
+              <div className='card-resident__header--circle' style={{backgroundColor : assignStatus(residents?.status)}}> </div>
               <p className='card-resident__header--status' >{residents?.status}</p>
               {/* <p className='card-resident__header--status' >{residents?.status}</p> */}
           </div>
         </header>
         <div className='card-resident__body'>
           <ul className='card-resident__body--datos'>
-            <li><span>Especies: </span>{residents?.species}</li>
-            <li><span>Origin: </span>{residents?.origin.name}</li>
-            <li><span>episode appearance: </span>{residents?.episode.length}</li>
+            <li><span style={{color : assignStatus(residents?.status)}}>Especies: </span>{residents?.species}</li>
+            <li><span style={{color : assignStatus(residents?.status)}}>Origin: </span>{residents?.origin.name}</li>
+            <li><span style={{color : assignStatus(residents?.status)}}>episode appearance: </span>{residents?.episode.length}</li>
           </ul>
         </div>
       </article>
